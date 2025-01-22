60f2ccc711b044fca1fa2e45a1d3d5ed57c7bb22
MH-Alyamani <mh.alymany@gmail.com>
2025-01-22 17:56:20 +0300

    help me fahd

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,8 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-   public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
@@ -16,6 +15,34 @@ public class MainMenu : MonoBehaviour
 
     public void Learn()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+        GameObject PlayButton = GameObject.FindWithTag("PlayButton");
+        GameObject QuitButton = GameObject.FindWithTag("QuitButton");
+        GameObject LearnButton = GameObject.FindWithTag("LearnButton");
+        GameObject HowToPlay = GameObject.FindWithTag("HowToPlay");
+        GameObject BackButton = GameObject.FindWithTag("BackButton");
+        Debug.Log(HowToPlay);
+
+        PlayButton.SetActive(false);
+        QuitButton.SetActive(false);
+        LearnButton.SetActive(false);
+
+        //HowToPlay.SetActive(true);
+        //BackButton.SetActive(true);
+    }
+
+    public void BackToMainMenu()
+    {
+        GameObject PlayButton = GameObject.FindWithTag("PlayButton");
+        GameObject QuitButton = GameObject.FindWithTag("QuitButton");
+        GameObject LearnButton = GameObject.FindWithTag("LearnButton");
+        GameObject HowToPlay = GameObject.FindWithTag("HowToPlay");
+        GameObject BackButton = GameObject.FindWithTag("BackButton");
+
+        if (HowToPlay) HowToPlay.SetActive(false);
+        if (BackButton) BackButton.SetActive(false);
+
+        if (PlayButton) PlayButton.SetActive(true);
+        if (QuitButton) QuitButton.SetActive(true);
+        if (LearnButton) LearnButton.SetActive(true);
     }
 }
