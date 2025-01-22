0c5bf044aa2f53e1a9f0fcd850c5307af3a5f480
FahdAlghamdi24 <fadealone44@gmail.com>
2025-01-22 18:26:16 +0300

    Learn how to play works

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Canvas mainMenuCanvas;
+    public Canvas learnHowToPlayCanvas;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,34 +18,13 @@ public class MainMenu : MonoBehaviour
 
     public void Learn()
     {
-        GameObject PlayButton = GameObject.FindWithTag("PlayButton");
-        GameObject QuitButton = GameObject.FindWithTag("QuitButton");
-        GameObject LearnButton = GameObject.FindWithTag("LearnButton");
-        GameObject HowToPlay = GameObject.FindWithTag("HowToPlay");
-        GameObject BackButton = GameObject.FindWithTag("BackButton");
-        Debug.Log(HowToPlay);
-
-        PlayButton.SetActive(false);
-        QuitButton.SetActive(false);
-        LearnButton.SetActive(false);
-
-        //HowToPlay.SetActive(true);
-        //BackButton.SetActive(true);
+        mainMenuCanvas.gameObject.SetActive(false);
+        learnHowToPlayCanvas.gameObject.SetActive(true);
     }
 
     public void BackToMainMenu()
     {
-        GameObject PlayButton = GameObject.FindWithTag("PlayButton");
-        GameObject QuitButton = GameObject.FindWithTag("QuitButton");
-        GameObject LearnButton = GameObject.FindWithTag("LearnButton");
-        GameObject HowToPlay = GameObject.FindWithTag("HowToPlay");
-        GameObject BackButton = GameObject.FindWithTag("BackButton");
-
-        if (HowToPlay) HowToPlay.SetActive(false);
-        if (BackButton) BackButton.SetActive(false);
-
-        if (PlayButton) PlayButton.SetActive(true);
-        if (QuitButton) QuitButton.SetActive(true);
-        if (LearnButton) LearnButton.SetActive(true);
+        mainMenuCanvas.gameObject.SetActive(true);
+        learnHowToPlayCanvas.gameObject.SetActive(false);
     }
 }
