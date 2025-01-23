71e01ccb8e08df8707f977069e15d7d4449c460f
FahdAlghamdi24 <fadealone44@gmail.com>
2025-01-23 12:20:43 +0300

    Can't fix it

--- Assets/Scripts/MainMenuMusic.cs
+++ Assets/Scripts/MainMenuMusic.cs
@@ -1,20 +1,21 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class MainMenuMusic : MonoBehaviour
 {
     public AudioSource src;
     public AudioClip clip;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        src.clip = clip;
-        src.Play();
-    }
+        // Ensure the AudioSource has the clip and starts playing
+        if (src.clip != clip)
+        {
+            src.clip = clip;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (!src.isPlaying)
+        {
+            src.Play();
+        }
     }
 }
